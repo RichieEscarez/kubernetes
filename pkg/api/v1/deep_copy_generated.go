d752d2d417e25ad26dc469f6234b6fe1a0605529
Clayton Coleman <ccoleman@redhat.com>
2015-10-03 16:38:49 -0400

    Support stdinOnce, which allows run-once STDIN injection
    This allows containers to wait for input before startup by listening
    on STDIN, and after STDIN is closed, continue running. Matches the
    Docker syntax.

--- pkg/api/v1/deep_copy_generated.go
+++ pkg/api/v1/deep_copy_generated.go
@@ -272,6 +272,7 @@ func deepCopy_v1_Container(in Container, out *Container, c *conversion.Cloner) e
 		out.SecurityContext = nil
 	}
 	out.Stdin = in.Stdin
+	out.StdinOnce = in.StdinOnce
 	out.TTY = in.TTY
 	return nil
 }
