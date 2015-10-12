2860eeca5602e692020831f706026bf6c32c97e7
Chao Xu <xuchao@google.com>
2015-10-12 11:18:50 -0700

    grep sed Experimental()

--- pkg/kubectl/describe.go
+++ pkg/kubectl/describe.go
@@ -883,7 +883,7 @@ type JobDescriber struct {
 }
 
 func (d *JobDescriber) Describe(namespace, name string) (string, error) {
-	job, err := d.client.Experimental().Jobs(namespace).Get(name)
+	job, err := d.client.Extensions().Jobs(namespace).Get(name)
 	if err != nil {
 		return "", err
 	}
@@ -923,7 +923,7 @@ type DaemonSetDescriber struct {
 }
 
 func (d *DaemonSetDescriber) Describe(namespace, name string) (string, error) {
-	dc := d.Experimental().DaemonSets(namespace)
+	dc := d.Extensions().DaemonSets(namespace)
 	pc := d.Pods(namespace)
 
 	daemon, err := dc.Get(name)
@@ -1254,7 +1254,7 @@ type HorizontalPodAutoscalerDescriber struct {
 }
 
 func (d *HorizontalPodAutoscalerDescriber) Describe(namespace, name string) (string, error) {
-	hpa, err := d.client.Experimental().HorizontalPodAutoscalers(namespace).Get(name)
+	hpa, err := d.client.Extensions().HorizontalPodAutoscalers(namespace).Get(name)
 	if err != nil {
 		return "", err
 	}
@@ -1420,7 +1420,7 @@ type DeploymentDescriber struct {
 }
 
 func (dd *DeploymentDescriber) Describe(namespace, name string) (string, error) {
-	d, err := dd.Experimental().Deployments(namespace).Get(name)
+	d, err := dd.Extensions().Deployments(namespace).Get(name)
 	if err != nil {
 		return "", err
 	}
